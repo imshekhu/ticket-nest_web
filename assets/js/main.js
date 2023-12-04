07562224a0d625235fee3399475ee7dedcefd835
imshekhu <51326063+imshekhu@users.noreply.github.com>
2023-12-03 23:12:27 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== ADD BLUR TO HEADER ===============*/
+function blurHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('blur-header'); else header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay:400,
+    //reset:true // Animation repeats
+
+})
+
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
+sr.reveal(`.home__card`, {delay:600, distance: '100px', interval:200})
+sr.reveal(`.about__data, .join__image`, {origin:'right'})
+sr.reveal(`.about__image, .join__data`, {origin:'left'})
+sr.reveal(`.about__card`, {interval:200})
+
+var myForm = document.getElementById('myForm');
+
+function showAlert(){
+    const name = document.getElementById('form-name'),
+      email = document.getElementById('form-email')
+alert('Thank you '+name.value+' for your interest in Ticket Nest. We will get back to you soon!')
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+for (i = 0; i < myForm.length; i++) {
+    if (myForm.elements[i].type != 'submit' && myForm.elements[i].type != 'button'){
+        myForm.elements[i].value = '';
+    }
+    
+
+}
+}
+myForm.addEventListener('submit', showAlert);
